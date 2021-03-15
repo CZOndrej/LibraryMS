fe5b2ffb9499ee2efd164b68256bc07c3d07f8f2
CZOndrej <on.krejci@gmail.com>
2021-03-15 19:45:07 +0100

    before user create

---
+++ LibraryMS/Controllers/AdminController.cs
@@ -0,0 +1,59 @@
+﻿using LibraryMS.Data;
+using LibraryMS.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryMS.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly UserManager<Account> _userManager;
+        private readonly AppIdentityDbContext _context;
+
+
+        public AdminController(UserManager<Account> userManager, AppIdentityDbContext context)
+        {
+            _userManager = userManager;
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public ViewResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(User user)
+        {
+            Address address = new Address
+            {
+                City = user.City,
+                Street = user.Street,
+                Zipcode = user.Zipcode,
+                State = user.State
+            };
+
+            _context.Add(address);
+
+            Person person = new Person
+            {
+                Email = user.Email,
+                Name = user.Name,
+                Address = address,
+
+            };
+            Account account = new Account
+            {
+                
+            };
+        }
+    }
+}
--- LibraryMS/Models/User.cs
+++ LibraryMS/Models/User.cs
@@ -7,5 +7,15 @@ namespace LibraryMS.Models
 {
     public class User
     {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Phone{ get; set; }
+        public int MyProperty { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string Zipcode { get; set; }
+        public string State { get; set; }
+
     }
 }
